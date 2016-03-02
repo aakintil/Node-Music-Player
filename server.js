69d3000e8178fe7d3bda61372e7a47da89188eea
Aderinsola <aderinsola_akintilo@ziba.com>
2016-03-01 20:34:12 -0800

    trying to fix laptop

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ var express = require('express'),
     request = require('request'), 
     app = express(); 
 
+var indexOf = [].indexOf || // no idea what this is
+    function( item ) { 
+        for ( var i = 0, l = this.length; i < l; i++ ) { 
+            if ( i in this && this[ i ] === item ) 
+                return i; 
+        } return -1; 
+    };
+
 // viewed at http://localhost:8080
 app.get( '/', function( req, res ) {
     res.sendFile( path.join( __dirname + '/index.html' ) );
@@ -28,15 +36,15 @@ function APIRequest() {
 }
 
 APIRequest.prototype = {
-    
+
     // get the facebook group posts
     getGroups: function( accessToken ) {
         var url = "https://graph.facebook.com/me/groups?access_token=" + accessToken;
         return request( url, function(error, response, body) {
-            
+
             posts = JSON.parse( body );
             console.log( posts ); 
-            
+
             if ( error ) {
                 console.log( "error ", error );
                 process.exit(); 
@@ -44,15 +52,15 @@ APIRequest.prototype = {
                 console.log( "posts error ", posts.error );
                 process.exit(); 
             }
-            
         })
     }
 }
-
 
 function getGroups( accessToken ) {
     var url = "https://graph.facebook.com/me/groups?access_token=" + accessToken;
 }
 
-console.log( "...working" ); 
-console.log( new APIRequest() ); 
+a = new APIRequest(); 
+token = 'CAACEdEose0cBABqO3wdcqkwCoffSHjQ9YgMVZBgbckZBRZAI3FoLUcc6W1mji8cX2fuGTo8l4BwkeHnwad6PVVPYGSc4S6TZBIC3ZCUcYXXjZAuTN5jxEs30ZB6KB97ZBUktp7ZBJwhTQW4q21akqbx8E9HuuQaLaKxHzSEzPuQNxkx2b1IPkXdrs9fcPU6IeP71rOxdvVlZBo2gZDZD'; 
+
+a.getGroups( token ); 

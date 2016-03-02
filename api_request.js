e91b61e64174ca65ce54127a2e829c7ffe70c5a5
Aderinsola <aderinsola_akintilo@ziba.com>
2016-03-02 14:03:05 -0800

    added the appropriate db

--- api_request.js
+++ api_request.js
@@ -1,38 +1,44 @@
 var express = require( 'express' ), 
     path = require( 'path' ), 
-    appDB = require( 'nedb' ), 
+    Datastore = require( 'nedb' ), 
     _ = require( 'underscore' ), 
     moment = require( 'moment' ),
     request = require( 'request' ), 
     app = express(); 
 
-var groupsDB = levelup( './appDB' ), 
-    indexOf = [].indexOf || function( item ) { 
-        for ( var i = 0, l = this.length; i < l; i++ ) { 
-            if ( i in this && this[ i ] === item ) 
-                return i; 
-        } return -1; 
-    };
-
-groupsDB.readStream()
-    .on( 'data', console.log )
-    .on('close', function () { console.log( '========================= FINISHED =========================' ) })
-
+var indexOf = [].indexOf || function( item ) { 
+    for ( var i = 0, l = this.length; i < l; i++ ) { 
+        if ( i in this && this[ i ] === item ) 
+            return i; 
+    } return -1; 
+}, 
+    appDB = new Datastore( { filename: 'db/app.db.js', autoload: true } ), 
+    _SMI = new Datastore( { filename: 'db/save.my.inbox.js', autoload: false } ), 
+    _FUNC = new Datastore( { filename: 'db/function.js', autoload: false } ), 
+    _SUPR = new Datastore( { filename: 'db/superior.js', autoload: false } ); 
 
 
+// Removing all documents with the 'match-all' query
+appDB.remove( {}, { multi: true }, function ( err, numRemoved ) {
+    console.log( "successfully removed: ", numRemoved ); 
+});
+
+// weird persistent access token hack that doesn't work 
+// https://graph.facebook.com/oauth/access_token?client_id=766241740105535&client_secret=9a58c535db3b7e9c8bd26b2179d91a85&grant_type=fb_exchange_token&fb_exchange_token=CAACEdEose0cBAH7zWnDqWlRSkc3xcI9aUjLSobttOA1o3662qhEEYn5X1zaUuH2UDTOZBhHUMlbEh2WfRRh8p3NiiLH554CilHnnvxbkcNJlalqOZB3tI0rZAv8ZBWhl9FdrLdLUfy2FMZB6ULQv8pth2sYstMXsR0MNgFv8B7AwaO0Tk5U7YiubPHAm9nObkS0qOqwHPYgZDZD
 
 function APIRequest() {
     this.res_data = []; 
     this.downloadSince = null;
     this.group_id = ""; 
+    this.groupIDs = []; 
     this.count = 0; 
-    this.accessToken = 'CAACEdEose0cBAKiTPsMpjHZCDvZAade8PoqHkd8NSpR5LV0eojv1jpNQE9shg0c9y9wzmnuSx3QOLZAFCpjZAGfiJpNXqYJk4Hb2bMZAcxz7zhKJ9utejepjWZB1I66VPeldw67VFmpp4iRZAMQgwXhtEBp4VmooDtKSZBw0KedAIJMnKSFGq2mw54lH0wV1iNXpXSjwqVkTjwZDZD'; 
+    this.accessToken = 'CAAK45I67Jz8BAHbG5dFFvGERKP5hjpZB3FH04UQ3p74ZCEUM6SDRWyBZBROLPhG89CpBZBdZAgAA98bBkjTXZApmeWVdQlqo6RzhdbS9mRkKiyT7JZCA34ZBL3EWSFixkAJ7IFliQHyEZAOwvnqspLibHIHkthIvcP1VfhkQvfJZB9xuxOruzBRY1R7cDvyhIqgvO2crdRWzJhZBQZDZD'; 
     this.posts = [];
     this.new_until = null; 
     this.getGroupURL = "https://graph.facebook.com/me/groups?access_token=" + this.accessToken; 
 }
-// https://graph.facebook.com/oauth/access_token?client_id=766241740105535&client_secret=9a58c535db3b7e9c8bd26b2179d91a85&grant_type=fb_exchange_token&fb_exchange_token=CAACEdEose0cBAH7zWnDqWlRSkc3xcI9aUjLSobttOA1o3662qhEEYn5X1zaUuH2UDTOZBhHUMlbEh2WfRRh8p3NiiLH554CilHnnvxbkcNJlalqOZB3tI0rZAv8ZBWhl9FdrLdLUfy2FMZB6ULQv8pth2sYstMXsR0MNgFv8B7AwaO0Tk5U7YiubPHAm9nObkS0qOqwHPYgZDZD
 
+// Main logic for all requests to put posts into a local db
 APIRequest.prototype = {
 
     // check to see if accessToken is still valid
@@ -43,6 +49,16 @@ APIRequest.prototype = {
     // if accessToken is invalid, we have to get a new one 
     getAccessToken: function() {
         // come back and finish 
+    }, 
+
+    // store all group ids for future reference 
+    storeAllGroupIDs: function( name, id, index ) {
+        this.groupIDs[ index ] = { name: name, id: id }; 
+    }, 
+
+    // get all group ids
+    getGroupIDs: function() {
+        return this.groupIDs; 
     }, 
 
     // get the facebook group posts
@@ -69,9 +85,9 @@ APIRequest.prototype = {
             // save it's id so we can create an actual req to SMI posts
             console.log( "\n1. successful graph explorer api call \n" ); 
             for ( var i in groups.data ) {
+                self.storeAllGroupIDs( groups.data[ i ].name, groups.data[ i ].id, i ); 
                 if ( groups.data[ i ].name === 'Save Mah Inbox' ) {
                     self.saveGroupId( groups.data[ i ].id );
-
                     console.log( "---- ASYNCHRONOUS CALL ... #3 is actually called before #2 ----" ); 
                     console.log( "2. successfully got group id...moving on... \n" ); 
                     var getPostsURL = self.timeParamUrl(); 
@@ -81,10 +97,13 @@ APIRequest.prototype = {
         })
     }, 
 
+    // save a specific group id
+    // might delete we have a db / storage of all MY groups and their ids
     saveGroupId: function( group_id ) {
         this.group_id = group_id; 
     }, 
 
+    // get all the music posts for a specific group
     getPostsForGroup: function( url ) {
         console.log( "\n 3. passing group id \n", this.group_id, "\n" ); 
 
@@ -123,6 +142,10 @@ APIRequest.prototype = {
 
             if ( numPostsFetched > 0 ) {
 
+                // save items into db
+                appDB.insert( [ posts.data[ 0 ], posts.data[ 1 ] ], function( err, newPost ) {
+                    console.log( '\n newly inserted post ', newPost )
+                }); 
                 var newUntil = moment( _.first( sortedUpdated( posts ) ) ).unix() - 1, 
                     newUntilDate = moment( _.first( sortedUpdated( posts ) ) ).format( "dddd, MMMM Do YYYY, h:mm:ss a" ), 
                     newURL = self.timeParamUrl( undefined, newUntil ); 
@@ -143,6 +166,8 @@ APIRequest.prototype = {
         })
     }, 
 
+    // edit the url to append Until Specific Date clause
+    // so that we can loop through facebook until there are no more posts in the group
     timeParamUrl: function( since, untilTime ) {
 
         var url = "https://graph.facebook.com/" + this.group_id + "/feed?limit=100&access_token=" + this.accessToken + "&fields=from,to,message,picture,link,name,caption,description,created_time,updated_time,likes,comments.limit(999)", 
@@ -183,5 +208,11 @@ function getGroups( accessToken ) {
 
 
 
-//a = new APIRequest(); 
+a = new APIRequest(); 
 //a.getGroups(); 
+
+//appDB.find( {}, function( err, posts ) {
+//    console.log( posts );
+//})
+
+a.getGroups(); 
